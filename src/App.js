1566914c06e6e576b22cbb2af022c9b25b633a5e
3ddesign <3ddesign@bk.ru>
2017-11-22 23:57:50 +0200

    add transfer func to component

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import Person from './Person/Person';
 class App extends Component {
     state = {
         persons: [
-            { name: 'Dmitry', age: 28 },
-            { name: 'Dmitry2', age: 31 },
+            { name: 'Dmitry', age: 30 },
+            { name: 'Dmitry1', age: 31 },
         ],
         otherState: 'some other state'
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         // console.log('test');
         this.setState({persons: [
-            { name: 'Scherbakov', age: 28 },
-            { name: 'Dmitry2', age: 31 }
+            { name: newName, age: 32 },
+            { name: 'Scherbakov', age: 33 }
+        ]
+        })
+    }
+
+    nameChangedHandler = (event) => {
+        this.setState({persons: [
+            { name: event.target.value, age: 32 },
+            { name: 'Scherbakov', age: 33 }
         ]
         })
     }
@@ -24,9 +32,11 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Hi from react App!</h1>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <button onClick={() => this.switchNameHandler('Dmitry2')}>Switch Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>test</Person>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, 'Dmitry')} changed={this.nameChangedHandler}
+                >test</Person>
             </div>
         );
     }
