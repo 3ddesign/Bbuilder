70582370a729ef6ceb26a97530c7c5e63c8573be
3ddesign <3ddesign@bk.ru>
2017-12-10 20:29:21 +0200

    add navigation and modal

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,7 +8,9 @@ const sideDrawer = (props) => {
 
     return (
         <div className={classes.SideDrawer}>
-            <Logo/>
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
             <nav>
                 <NavigationItems/>
             </nav>
@@ -17,3 +19,4 @@ const sideDrawer = (props) => {
 };
 
 export default sideDrawer;
+
