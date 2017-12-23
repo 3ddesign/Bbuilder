1e18f269de423edcb752f547b1749ec568932779
3ddesign <3ddesign@bk.ru>
2017-12-23 15:53:37 +0200

    add utility

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-
 import thunk from 'redux-thunk';
-
-
-import {createStore, applyMiddleware, compose } from 'redux';
+import {createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(burgerBuilderReducer,composeEnhancers(
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(thunk)
 ));
 
@@ -26,5 +29,5 @@ const app = (
     </Provider>
 );
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render( app, document.getElementById( 'root' ) );
 registerServiceWorker();
